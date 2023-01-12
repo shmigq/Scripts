1dd5969c68bdd6dfb31f954ae8dd84dd83a46cfc
shmigq <120087624+shmigq@users.noreply.github.com>
2023-01-12 18:40:13 +0600

    Update KladovkaShmiga.lua

--- KladovkaShmiga.lua
+++ KladovkaShmiga.lua
@@ -1,4 +1,4 @@
-        local DarkraiX = loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingScripter/Kavo-Ui/main/Darkrai%20Ui", true))()
+local DarkraiX = loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingScripter/Kavo-Ui/main/Darkrai%20Ui", true))()
 
         local Library = DarkraiX:Window("KladovkaDH","","",Enum.KeyCode.RightControl);
 
@@ -235,14 +235,115 @@
 
         end)
         Tab1:Button("Bullet Proof",function()
-            Instance.new("Folder",game:GetService("Players").LocalPlayer.Character).Name = ("FULLY_LOADED_CHAR")
-                    game:GetService("Players").LocalPlayer.Character:WaitForChild("BodyEffects"):WaitForChild("Dead"):Remove()
-                    Instance.new("BoolValue",game:GetService("Players").LocalPlayer.Character:WaitForChild("BodyEffects")).Name = ("Dead")
-                    repeat wait() until game:GetService("Players").LocalPlayer.Character:WaitForChild("BodyEffects"):findFirstChild("Dead")
-                    game:GetService("Players").LocalPlayer.Character:WaitForChild("BodyEffects"):WaitForChild("Cuff").Value = false
-                    game:GetService("Players").LocalPlayer.Character:findFirstChild("BodyEffects"):WaitForChild("Armor"):Destroy()
-        game:GetService("ReplicatedStorage").MainEvent:FireServer("UpdateMousePos", game:GetService("Players").LocalPlayer:GetMouse().Hit.Position)
+            local plr = game.Players.LocalPlayer
+local mouse = plr:GetMouse()
 
+localplayer = plr
+
+if workspace:FindFirstChild("Core") then
+workspace.Core:Destroy()
+end
+
+local Core = Instance.new("Part")
+Core.Name = "Core"
+Core.Size = Vector3.new(0.05, 0.05, 0.05)
+
+spawn(function()
+Core.Parent = workspace
+local Weld = Instance.new("Weld", Core)
+Weld.Part0 = Core
+Weld.Part1 = localplayer.Character.LowerTorso
+Weld.C0 = CFrame.new(0, 0, 0)
+end)
+
+workspace:WaitForChild("Core")
+
+local torso = workspace.Core
+flying = true
+local speed=10
+local keys={a=false,d=false,w=false,s=false}
+local e1
+local e2
+local function start()
+local pos = Instance.new("BodyPosition",torso)
+local gyro = Instance.new("BodyGyro",torso)
+pos.Name="EPIXPOS"
+pos.maxForce = Vector3.new(math.huge, math.huge, math.huge)
+pos.position = torso.Position
+gyro.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+gyro.cframe = torso.CFrame
+repeat
+wait()
+localplayer.Character.Humanoid.PlatformStand=true
+local new=gyro.cframe - gyro.cframe.p + pos.position
+if not keys.w and not keys.s and not keys.a and not keys.d then
+speed=5
+end
+if keys.w then
+new = new + workspace.CurrentCamera.CoordinateFrame.lookVector * speed
+speed=speed+0
+end
+if keys.s then
+new = new - workspace.CurrentCamera.CoordinateFrame.lookVector * speed
+speed=speed+0
+end
+if keys.d then
+new = new * CFrame.new(speed,0,0)
+speed=speed+0
+end
+if keys.a then
+new = new * CFrame.new(-speed,0,0)
+speed=speed+0
+end
+if speed>10 then
+speed=5
+end
+pos.position=new.p
+if keys.w then
+gyro.cframe = workspace.CurrentCamera.CoordinateFrame*CFrame.Angles(-math.rad(speed*0),0,0)
+elseif keys.s then
+gyro.cframe = workspace.CurrentCamera.CoordinateFrame*CFrame.Angles(math.rad(speed*0),0,0)
+else
+gyro.cframe = workspace.CurrentCamera.CoordinateFrame
+end
+until flying == false
+if gyro then gyro:Destroy() end
+if pos then pos:Destroy() end
+flying=false
+localplayer.Character.Humanoid.PlatformStand=false
+speed=10
+end
+e1=mouse.KeyDown:connect(function(key)
+if not torso or not torso.Parent then flying=false e1:disconnect() e2:disconnect() return end
+if key=="w" then
+keys.w=true
+elseif key=="s" then
+keys.s=true
+elseif key=="a" then
+keys.a=true
+elseif key=="d" then
+keys.d=true
+elseif key=="x" then
+if flying==true then
+flying=false
+else
+flying=true
+start()
+end
+end
+end)
+e2=mouse.KeyUp:connect(function(key)
+if key=="w" then
+keys.w=false
+elseif key=="s" then
+keys.s=false
+elseif key=="a" then
+keys.a=false
+elseif key=="d" then
+keys.d=false
+end
+end)
+start()
         end)
         Tab1:Button("Color Correction",function()
             -- // Created by rice
@@ -352,10 +453,6 @@
                         end)
                         Tab3:Button("Auto Block",function()
                             --[[
-
-
-
-
     ]]--
 
 
@@ -627,114 +724,22 @@ if LocalPlayer.Character then
     game.Players.LocalPlayer.Character:BreakJoints()
 end
                                 end)
-                                Tab1:Button("Fly (X)",function()
-                                    local plr = game.Players.LocalPlayer
-local mouse = plr:GetMouse()
-
-localplayer = plr
-
-if workspace:FindFirstChild("Core") then
-workspace.Core:Destroy()
-end
-
-local Core = Instance.new("Part")
-Core.Name = "Core"
-Core.Size = Vector3.new(0.05, 0.05, 0.05)
-
-spawn(function()
-Core.Parent = workspace
-local Weld = Instance.new("Weld", Core)
-Weld.Part0 = Core
-Weld.Part1 = localplayer.Character.LowerTorso
-Weld.C0 = CFrame.new(0, 0, 0)
-end)
-
-workspace:WaitForChild("Core")
-
-local torso = workspace.Core
-flying = true
-local speed=10
-local keys={a=false,d=false,w=false,s=false}
-local e1
-local e2
-local function start()
-local pos = Instance.new("BodyPosition",torso)
-local gyro = Instance.new("BodyGyro",torso)
-pos.Name="EPIXPOS"
-pos.maxForce = Vector3.new(math.huge, math.huge, math.huge)
-pos.position = torso.Position
-gyro.maxTorque = Vector3.new(9e9, 9e9, 9e9)
-gyro.cframe = torso.CFrame
-repeat
-wait()
-localplayer.Character.Humanoid.PlatformStand=true
-local new=gyro.cframe - gyro.cframe.p + pos.position
-if not keys.w and not keys.s and not keys.a and not keys.d then
-speed=5
-end
-if keys.w then
-new = new + workspace.CurrentCamera.CoordinateFrame.lookVector * speed
-speed=speed+0
-end
-if keys.s then
-new = new - workspace.CurrentCamera.CoordinateFrame.lookVector * speed
-speed=speed+0
-end
-if keys.d then
-new = new * CFrame.new(speed,0,0)
-speed=speed+0
-end
-if keys.a then
-new = new * CFrame.new(-speed,0,0)
-speed=speed+0
-end
-if speed>10 then
-speed=5
-end
-pos.position=new.p
-if keys.w then
-gyro.cframe = workspace.CurrentCamera.CoordinateFrame*CFrame.Angles(-math.rad(speed*0),0,0)
-elseif keys.s then
-gyro.cframe = workspace.CurrentCamera.CoordinateFrame*CFrame.Angles(math.rad(speed*0),0,0)
-else
-gyro.cframe = workspace.CurrentCamera.CoordinateFrame
-end
-until flying == false
-if gyro then gyro:Destroy() end
-if pos then pos:Destroy() end
-flying=false
-localplayer.Character.Humanoid.PlatformStand=false
-speed=10
-end
-e1=mouse.KeyDown:connect(function(key)
-if not torso or not torso.Parent then flying=false e1:disconnect() e2:disconnect() return end
-if key=="w" then
-keys.w=true
-elseif key=="s" then
-keys.s=true
-elseif key=="a" then
-keys.a=true
-elseif key=="d" then
-keys.d=true
-elseif key=="x" then
-if flying==true then
-flying=false
-else
-flying=true
-start()
-end
-end
-end)
-e2=mouse.KeyUp:connect(function(key)
-if key=="w" then
-keys.w=false
-elseif key=="s" then
-keys.s=false
-elseif key=="a" then
-keys.a=false
-elseif key=="d" then
-keys.d=false
-end
-end)
-start()
+                                Tab4:Button("Aimbot",function()
+                                    getgenv().RecurringPoint = "UpperTorso"
+getgenv().Hitbox = "UpperTorso"
+getgenv().Keybind = "e"
+getgenv().AimbotStrengthAmount = 0.0335
+getgenv().PredictionAmount = 10
+getgenv().Radius = 25
+getgenv().UsePrediction = true
+getgenv().AimbotStrength = true
+getgenv().FirstPerson = true
+getgenv().ThirdPerson = true
+getgenv().TeamCheck = false
+getgenv().Enabled = true
+ 
+ 
+-- // main script use with silent aim / / -- 
+ 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tenaaki/GenericAimbot/main/v1.0.0"))()
                                 end)
